53817c14ce41b0d6387b86d1991b90a9d0526a15
Carlos Pérez <carlos.perez@gmail.com>
2025-09-25 23:16:10 -0500

    Jheanpiere Cambio 3

--- TA-S4/Entities/Album.cs
+++ TA-S4/Entities/Album.cs
@@ -34,39 +34,3 @@ namespace TA_S4.Entities
             }
         }
     }
-
-
-
-}
-
-
-namespace DiscoApp
-{
-    public class Album
-    {
-        public string Codigo { get; set; }
-        public string Nombre { get; set; }
-        public List<Cancion> Canciones { get; private set; }
-
-        public Album(string codigo, string nombre)
-        {
-            Codigo = codigo;
-            Nombre = nombre;
-            Canciones = new List<Cancion>();
-        }
-
-        public bool AgregarCancion(Cancion c)
-        {
-            // No permitir canciones repetidas dentro del mismo álbum (por código)
-            if (Canciones.Any(x => x.Codigo == c.Codigo)) return false;
-            Canciones.Add(c);
-            return true;
-        }
-
-        public override string ToString()
-        {
-            return $"{Codigo} - {Nombre} ({Canciones.Count} canciones)";
-        }
-    }
-}
-
