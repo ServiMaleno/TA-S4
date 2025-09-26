68393e9aa379e69b5205cc7734e49686948f56dc
Carlos Pérez <carlos.perez@gmail.com>
2025-09-25 23:12:14 -0500

    Jheanpiere Modifico

--- TA-S4/Entities/Cancion.cs
+++ TA-S4/Entities/Cancion.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace TA_S4.Entities
 {
-    public class Cancion
-    {
-        //Constructor
-        public Cancion() { } 
-        // Propiedades de los atributos
-        public string CodigoCancion { get; set; }
-        public string NombreCancion { get; set; }
-        public float Duracion { get; set; }
-    }
+
+        public class Cancion
+        {
+            public string Codigo { get; set; }
+            public string Nombre { get; set; }
+
+            // Duración en minutos (double) o en segundos según preferencia; aquí usamos minutos.
+            public double DuracionMinutos { get; set; }
+
+            public Cancion(string codigo, string nombre, double duracionMinutos)
+            {
+                Codigo = codigo;
+                Nombre = nombre;
+                DuracionMinutos = duracionMinutos;
+            }
+
+            public override string ToString()
+            {
+                return $"{Codigo} - {Nombre} ({DuracionMinutos} min)";
+            }
+        }
+
 }
